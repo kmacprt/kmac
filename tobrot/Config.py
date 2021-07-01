778ee35d81fbcc5c6997a34a12b1c9826824650d
KAJ <72274248+kmacprt@users.noreply.github.com>
2021-07-01 06:08:05 -0700

    Update and rename config.py to Config.py

---
+++ tobrot/Config.py
@@ -0,0 +1,12 @@
+from tobrot.sample_config import Config
+
+class Config(Config):
+TG_BOT_TOKEN = "1859095650:AAEnjYdd1dOheKpbIvkgYAPzBUtWyvc72u0"
+APP_ID = 1971546
+API_HASH = "b994bccad6118cd6fb6d091b9bbfe5cf"
+AUTH_CHANNEL = -1001227974798
+R_CLONE_CONF_URI = "https://t.me/c/1535672213/6"TG_BOT_TOKEN = "1859095650:AAEnjYdd1dOheKpbIvkgYAPzBUtWyvc72u0"
+APP_ID = 1971546
+API_HASH = "b994bccad6118cd6fb6d091b9bbfe5cf"
+AUTH_CHANNEL = -1001227974798
+R_CLONE_CONF_URI = "https://t.me/c/1535672213/6"
